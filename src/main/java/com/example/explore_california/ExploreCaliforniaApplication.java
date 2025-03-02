b7511e66be8a664159e7ec629cf9ebebf64a6d73
Sahil Rajput <rajputsahil.2204@gmail.com>
2025-03-02 22:19:12 +0530

    Project Started

---
+++ src/main/java/com/example/explore_california/ExploreCaliforniaApplication.java
@@ -0,0 +1,13 @@
+package com.example.explore_california;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExploreCaliforniaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExploreCaliforniaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/explore_california/ExploreCaliforniaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.explore_california;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExploreCaliforniaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
