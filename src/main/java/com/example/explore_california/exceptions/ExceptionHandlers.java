891772765425718e2c0cbe2398998e33dc5b7457
Sahil Rajput <rajputsahil.2204@gmail.com>
2025-04-01 13:41:43 +0530

    Global Exception Handler Developed

--- src/main/java/com/example/explore_california/exceptions/ExceptionHandlers.java
+++ src/main/java/com/example/explore_california/exceptions/ExceptionHandlers.java
@@ -1,18 +1,34 @@
 package com.example.explore_california.exceptions;
 
+import jakarta.validation.ConstraintViolationException;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ProblemDetail;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import java.util.NoSuchElementException;
 
 @ControllerAdvice
-public class ExceptionHandlers {
+public class ExceptionHandlers extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler(NoSuchElementException.class)
-    @ResponseStatus(HttpStatus.NOT_FOUND)
-    public String handleNoSuchElementException(NoSuchElementException noSuchElementException) {
-        return noSuchElementException.getMessage();
+    public final ResponseEntity<Object> handleNoSuchElementException(NoSuchElementException noSuchElementException, WebRequest webRequest) {
+        ProblemDetail problemDetail = ProblemDetail.forStatusAndDetail(HttpStatus.NOT_FOUND, noSuchElementException.getMessage());
+        return createResponseEntity(problemDetail, null, HttpStatus.NOT_FOUND, webRequest);
+    }
+
+    @ExceptionHandler(ConstraintViolationException.class)
+    public final ResponseEntity<Object> handleConstraintViolationException(ConstraintViolationException constraintViolationException, WebRequest webRequest) {
+        ProblemDetail problemDetail = ProblemDetail.forStatusAndDetail(HttpStatus.BAD_REQUEST, constraintViolationException.getMessage());
+        return createResponseEntity(problemDetail, null, HttpStatus.BAD_REQUEST, webRequest);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public final ResponseEntity<Object> handleGlobalException(Exception ex, WebRequest request) {
+        ProblemDetail problemDetail = ProblemDetail.forStatusAndDetail(HttpStatus.INTERNAL_SERVER_ERROR, ex.getMessage());
+        return createResponseEntity(problemDetail, null, HttpStatus.INTERNAL_SERVER_ERROR, request);
     }
 }
