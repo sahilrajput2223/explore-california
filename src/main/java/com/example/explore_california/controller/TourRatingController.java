4353653843d16242179149619c6ecde69d0e39e7
Sahil Rajput <rajputsahil.2204@gmail.com>
2025-07-01 15:13:41 +0530

    Swagger Basic Documentation Added

--- src/main/java/com/example/explore_california/controller/TourRatingController.java
+++ src/main/java/com/example/explore_california/controller/TourRatingController.java
@@ -1,8 +1,9 @@
 package com.example.explore_california.controller;
 
 import com.example.explore_california.dtos.RatingCreateReqDTO;
-import com.example.explore_california.models.TourRating;
 import com.example.explore_california.service.TourRatingService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -14,6 +15,7 @@ import java.util.Map;
 @Slf4j
 @RestController
 @RequestMapping(path = "/tours/{tourId}/ratings")
+@Tag(name = "Tour Rating APIs", description = "Tour Rating APIs to Manage Rating By Tour and Customers")
 public class TourRatingController {
 
     @Autowired
@@ -21,36 +23,42 @@ public class TourRatingController {
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
+    @Operation(description = "Add Tour Rating")
     private void createTourRating(@PathVariable(value = "tourId") int tourId, @RequestBody RatingCreateReqDTO ratingCreateReqDTO) {
         log.info("POST : /tours/{}/ratings", tourId);
         tourRatingService.createTourRating(tourId, ratingCreateReqDTO.getCustomerId(), ratingCreateReqDTO.getScore(), ratingCreateReqDTO.getComment());
     }
 
     @PutMapping
+    @Operation(description = "Edit Tour Rating")
     private void updateTourRating(@PathVariable(value = "tourId") int tourId, @RequestBody RatingCreateReqDTO ratingCreateReqDTO) {
         log.info("PUT : /tours/{}/ratings", tourId);
         tourRatingService.updateTourRating(tourId, ratingCreateReqDTO);
     }
 
     @PatchMapping
+    @Operation(description = "Partial Tour Rating")
     private void updateTourRatingWithPatch(@PathVariable(value = "tourId") int tourId, @RequestBody RatingCreateReqDTO ratingCreateReqDTO) {
         log.info("PATCH : /tours/{}/ratings", tourId);
         tourRatingService.updateTourRatingWithPatch(tourId, ratingCreateReqDTO);
     }
 
     @GetMapping
+    @Operation(description = "Get All Ratings By Tour")
     private List<RatingCreateReqDTO> getAllRatingsForTour(@PathVariable(value = "tourId") int tourId) {
         log.info("GET : /tours/{}/ratings", tourId);
         return tourRatingService.getAllRatingsForTour(tourId);
     }
 
     @DeleteMapping("/{customerId}")
+    @Operation(description = "Delete Tour Rating by Customer Id")
     public void deleteTourRating(@PathVariable(value = "tourId") int tourId, @PathVariable(value = "customerId") int customerId) {
         log.info("DELETE : /tours/{}/ratings/{}", tourId, customerId);
         tourRatingService.deleteTourRating(tourId, customerId);
     }
 
     @GetMapping(value = "/average")
+    @Operation(description = "Get Average Tour Rating")
     private Map<String, Double> getAverageTourRatings(@PathVariable(value = "tourId") int tourId) {
         log.info("GET : /tours/{}/ratings/average", tourId);
         return tourRatingService.getAverageTourRatings(tourId);
